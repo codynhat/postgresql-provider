b39f1d92634cf573cd75609f04588aae88a399d4
Cody Hatfield <cody@hatfieldworks.com>
2016-08-22 20:24:16 -0700

    Changed driver package path

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporPostgreSQL",
     dependencies: [
-   		 .Package(url: "https://github.com/vapor/postgresql-driver.git", majorVersion: 0, minor: 2),
+   		 .Package(url: "https://github.com/codynhat/postgresql-driver.git", majorVersion: 0, minor: 2),
    		 .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 13)
     ]
 )
